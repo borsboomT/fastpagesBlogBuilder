7e892ed1b2fe1bf3c4567b17b68ce4d01a188d53
@borsboom.tory <borsboom.tory@gmail.com>
2022-01-23 07:47:10 -0800

    First post

--- _pages/about.md
+++
@@ -1,7 +0,0 @@
----
-layout: page
-title: My Portfolio
-permalink: /about/
----
-
-
---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+
+function navFunc() {
+	var element1 = document.getElementById("#navBar");
+	var element2 = document.getElementById("#navButton");
+	element1.classList.toggle('navbar')
+    element1.classList.toggle('navbar-mobile')
+    element2.classList.toggle('bi-list')
+    element2.classList.toggle('bi-x')
+  }
+
